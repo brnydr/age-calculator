0338f21a31da8711a4a3a5aceff1a5c6be970860
brnydr <brianwilliamyoder@gmail.com>
2023-07-27 11:11:29 -0700

    include other planets in 3rd test

--- _tests_/ageCalculator.test.js
+++ _tests_/ageCalculator.test.js
@@ -18,6 +18,11 @@ beforeEach(() => {
 
   test("should correctly calculate the age of a person in Mercury years", () => {
     expect(newAgeCalculator.calculatePlanetAge("mercury")).toEqual("8.88");
+    expect(newAgeCalculator.calculatePlanetAge("venus")).toEqual("22.94");
+    expect(newAgeCalculator.calculatePlanetAge("earth")).toEqual("37.00");
+    expect(newAgeCalculator.calculatePlanetAge("mars")).toEqual("69.56");
+    expect(newAgeCalculator.calculatePlanetAge("jupiter")).toEqual("438.82");
+
   });
 
  
