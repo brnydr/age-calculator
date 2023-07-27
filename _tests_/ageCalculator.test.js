05c3fac0c57036f80145a47ddaf79055abe4d7d5
brnydr <brianwilliamyoder@gmail.com>
2023-07-27 10:30:32 -0700

    first test good fail

---
+++ _tests_/ageCalculator.test.js
@@ -0,0 +1,8 @@
+import AgeCalculator from ".././src/ageCalculator.js";
+
+describe("AgeCalculator", () => {
+  test("should correctly create an AgeCalculator object with an age property", () => {
+    const newAgeCalculator = new AgeCalculator(37);
+    expect(newAgeCalculator.age).isEqualTo(37);
+  });
+});
