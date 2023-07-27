9fcf203662acf025d9853cad45d9565f6a1a9433
brnydr <brianwilliamyoder@gmail.com>
2023-07-27 10:49:26 -0700

    add planetYears property

--- src/ageCalculator.js
+++ src/ageCalculator.js
@@ -2,5 +2,11 @@
 export default class AgeCalcuator {
   constructor(age) {
     this.age = age;
+    this.planetYears = {
+      mercury: .24,
+      venus: .62,
+      mars: 1.88,
+      jupiter: 11.86
+    };
   };
  };
