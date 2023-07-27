50d93e26971264b96344fa4c202c9971cf78cce4
brnydr <brianwilliamyoder@gmail.com>
2023-07-27 11:49:53 -0700

    refactor test to match corrected calculations

--- _tests_/ageCalculator.test.js
+++ _tests_/ageCalculator.test.js
@@ -17,20 +17,29 @@ beforeEach(() => {
   });
 
   test("should correctly calculate the age of a person in Mercury years", () => {
-    expect(newAgeCalculator.calculatePlanetAge("mercury")).toEqual("8.88");
-    expect(newAgeCalculator.calculatePlanetAge("venus")).toEqual("22.94");
+    expect(newAgeCalculator.calculatePlanetAge("mercury")).toEqual("154.17");
+    expect(newAgeCalculator.calculatePlanetAge("venus")).toEqual("59.68");
     expect(newAgeCalculator.calculatePlanetAge("earth")).toEqual("37.00");
-    expect(newAgeCalculator.calculatePlanetAge("mars")).toEqual("69.56");
-    expect(newAgeCalculator.calculatePlanetAge("jupiter")).toEqual("438.82");
+    expect(newAgeCalculator.calculatePlanetAge("mars")).toEqual("19.68");
+    expect(newAgeCalculator.calculatePlanetAge("jupiter")).toEqual("3.12");
 
   });
 
   test("should correctly calculate the number of years passed on a planet since a past birthday", () => {
-    expect(newAgeCalculator.calculateYearsPassed(36, "mercury")).toEqual("0.24");
-    expect(newAgeCalculator.calculateYearsPassed(33, "venus")).toEqual("2.48");
+    expect(newAgeCalculator.calculateYearsPassed(36, "mercury")).toEqual("4.17");
+    expect(newAgeCalculator.calculateYearsPassed(33, "venus")).toEqual("6.45");
     expect(newAgeCalculator.calculateYearsPassed(30, "earth")).toEqual("7.00");
-    expect(newAgeCalculator.calculateYearsPassed(27, "mars")).toEqual("18.80");
-    expect(newAgeCalculator.calculateYearsPassed(24, "jupiter")).toEqual("154.18");
+    expect(newAgeCalculator.calculateYearsPassed(27, "mars")).toEqual("5.32");
+    expect(newAgeCalculator.calculateYearsPassed(24, "jupiter")).toEqual("1.10");
+  });
+
+  test("should correctly calculate the number of years on a planet until a future birthday", () => {
+    expect(newAgeCalculator.calculateYearsUntil(40, "mercury")).toEqual("0.72");
+    expect(newAgeCalculator.calculateYearsUntil(42, "venus")).toEqual("3.10");
+    expect(newAgeCalculator.calculateYearsUntil(44, "earth")).toEqual("7.00");
+    expect(newAgeCalculator.calculateYearsUntil(46, "mars")).toEqual("16.92");
+    expect(newAgeCalculator.calculateYearsUntil(48, "jupiter")).toEqual("40.74");
+
   });
 
 });
