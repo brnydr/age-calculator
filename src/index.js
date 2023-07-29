b7c45d1c6d2d0e00b97e30c134465afee0f18df3
brnydr <brianwilliamyoder@gmail.com>
2023-07-28 22:50:21 -0700

    add images, update styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+// import {AgeCalculator} from "./ageCalculator.js";
 
-//import {AgeCalculator} from "./ageCalculator.js";
 
+
+// function handleFormSubmission() {
+//   let age = document.getElementById('age').value;
+//   let pastAge = document.getElementById('pastAge').value;
+//   let futureAge = document.getElementById('futureAge').value;
+//   let newAge = new AgeCalculator(age);
+//   for (let planet in newAge.planetYears) {
+   
+//   }
+  
+// }
+
+
+
+// window.addEventListener('load', handFormSubmission);
